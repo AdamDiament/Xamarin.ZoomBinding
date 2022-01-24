343bbd0737956f6e15d3b33a57dd22d36ad7dfdc
Adam Diament <adam@clevertree.io>
2022-01-24 17:11:09 +0200

    5.9.1.3674 building

---
+++ src/CommonLib/Additions/ZmMutableLiveDataComparator.extend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AndroidX.Lifecycle;
+using Object = Java.Lang.Object;
+
+namespace US.Zoom.Androidlib.Mvvm.Data
+{
+    public partial class ZmMutableLiveDataComparator
+    {
+        public int Compare(Java.Lang.Object? o1, Object? o2)
+        {
+            return Compare((ZmMutableLiveData)o1, (ZmMutableLiveData)o2);
+        }
+    }
+}
